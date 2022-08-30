7dfc332abadce0f720dc225fc4700a4698202f33
amarjeetk06 <amarjeetk406@gmail.com>
2022-08-30 21:41:34 +0530

    initial commit

---
+++ src/component/Basics/MenuApi.js
@@ -0,0 +1,129 @@
+const Menu=[
+    {
+     id:1,
+     image: "../images/sweets.jpg",
+     name: "sweets",
+     category: "breakfast",
+     price: "12rs",
+     description:
+     "i love maggie,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+
+    {
+        id:2,
+        image:"../images/momo.webp",
+        name:"momo",
+        category:"dinner",
+        price:"12rs",
+        description:
+        "i love momo,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+       },
+
+       {
+        id:3,
+        image:"../images/corn.jpg",
+        name:"corn",
+        category:"breakfast",
+        price:"20rs",
+        description:
+        "i love corn,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+       },
+
+
+       {
+        id:4,
+        image:"../images/chola.jpg",
+        name:"chola",
+        category:"lunch",
+        price:"30rs",
+        description:
+        "i love chola,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+       },
+
+
+       {
+        id:5,
+        image:"../images/allu pakoda.jpg",
+        name:"allu pakoda",
+        category:"evening",
+        price:"10rs",
+        description:
+        "i love allu pakoda, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+       },
+
+
+       {
+        id:6,
+        image:"../images/maggie.jpg",
+        name:"maggie",
+        category:"breakfast",
+        price:"12rs",
+        description:
+        "i love maggie,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+       },
+
+
+       {
+        id:7,
+        image:"../images/non veg.jpg",
+        name:"non veg",
+        category:"lunch",
+        price:"40rs",
+        description:
+        "i love non veg, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+       },
+
+
+       {
+        id:8,
+        image:"../images/dinner.jpg",
+        name:"cake",
+        category:"evening",
+        price:"22rs",
+        description:
+        "i love foods,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+       },
+
+
+       {
+        id:9,
+        image:"../images/lunch.jpg",
+        name:"chawal plate",
+        category:"dinner",
+        price:"40rs",
+        description:
+        "i love dinner,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+       },
+
+       {
+        id:10,
+        image:"../images/pizza.webp",
+        name:"pizza",
+        category:"evening",
+        price:"30rs",
+        description:
+        "i love dinner,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+       },
+
+       {
+        id:11,
+        image:"../images/evening snaks.jpg",
+        name:"almonds",
+        category:"evening",
+        price:"24rs",
+        description:
+        "i love dinner,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+       },
+
+       {
+        id:12,
+        image:"../images/brakefast.jpg",
+        name:"bread",
+        category:"lunch",
+        price:"10rs",
+        description:
+        "i love dinner,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+       },
+];
+
+export default Menu;
